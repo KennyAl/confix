7cec001f32fba7c989c29f160b726e0521e9e78e
Kenny Albes <albes@sra.uni-hannover.de>
2025-06-25 10:39:21 +0200

    gnome: stuff was moved out of the xserver namespace

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -11,8 +11,8 @@
   programs.xwayland.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Not sure if this even works. Discord crashes stil while screensharing...
   xdg.portal = {
