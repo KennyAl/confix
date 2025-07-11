76635cab8ede2d1f1306ed7ea434b7f2853c3df9
Kenny Albes <KennyA@posteo.de>
2025-07-11 19:16:50 +0200

    homepc: add obs-studio with virtual webcam support

--- hosts/homepc/default.nix
+++ hosts/homepc/default.nix
@@ -17,6 +17,15 @@
     boot.supportedFilesystems = [ "ntfs" ];
     boot.kernelPackages = pkgs.linuxPackages_latest;
 
+    boot = {
+      extraModulePackages = with config.boot.kernelPackages; [
+        v4l2loopback
+      ];
+      extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      '';
+    };
+
     hardware.graphics.enable = true;
     hardware.nvidia.nvidiaSettings = true;
     hardware.nvidia = {
@@ -58,6 +67,7 @@
     programs.steam.gamescopeSession.enable = true;
     environment.systemPackages = with pkgs; [
       keymapp
+      obs-studio
       protonvpn-gui
       tidal-hifi
       vdhcoapp
